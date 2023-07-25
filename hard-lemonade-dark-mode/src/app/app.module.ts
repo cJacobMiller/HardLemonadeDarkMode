c1f774ee72fbea2b5208e9151ad777d30f299d77
Vondrak, Mike <mike.vondrak@charter.com>
2023-07-25 15:01:35 -0600

    sticky footer layout

--- hard-lemonade-dark-mode/src/app/app.module.ts
+++ hard-lemonade-dark-mode/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
+import { SiteFooterComponent } from './components/site-footer/site-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiteHeaderComponent
+    SiteHeaderComponent,
+    SiteFooterComponent
   ],
   imports: [
     BrowserModule,
