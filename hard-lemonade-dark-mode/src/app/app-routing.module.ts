0a678b693625e62f370f7d4ee3eadc244032e002
Vondrak, Mike <mike.vondrak@charter.com>
2023-07-26 10:12:04 -0600

    about page route

--- hard-lemonade-dark-mode/src/app/app-routing.module.ts
+++ hard-lemonade-dark-mode/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './components/pages/page-home/page-home.component';
+import { PageAboutComponent } from './components/pages/page-about/page-about.component';
 
 const routes: Routes = [
   { path: 'home', component: PageHomeComponent },
+  { path: 'about', component: PageAboutComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
