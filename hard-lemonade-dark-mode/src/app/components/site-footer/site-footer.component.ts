bd4e451c6e18383b104a5985bc323ed7ac0af958
Vondrak, Mike <mike.vondrak@charter.com>
2023-07-26 19:02:42 -0600

    fonts

--- hard-lemonade-dark-mode/src/app/components/site-footer/site-footer.component.ts
+++ hard-lemonade-dark-mode/src/app/components/site-footer/site-footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeImage, ThemeImageMap, emptyThemeImage } from 'src/app/models/image';
+import { Themes } from 'src/app/models/themes';
 
 @Component({
   selector: 'app-site-footer',
   templateUrl: './site-footer.component.html',
   styleUrls: ['./site-footer.component.scss']
 })
-export class SiteFooterComponent {
+export class SiteFooterComponent implements OnInit {
 
+  public logoConfig: ThemeImage = emptyThemeImage;
+
+  ngOnInit(): void {
+    const logoSources: ThemeImageMap = new Map([
+      [Themes.LIGHT, 'assets/images/Charter_R_Logo_White_RGB.png'],
+      [Themes.DARK, 'assets/images/Charter_R_Logo_Black_RGB.png'],
+    ]);
+
+    this.logoConfig = {
+      alt: 'Spectrum Logo',
+      sources: logoSources,
+      width: 'clamp(6rem, 7vw, 10rem)', // TODO how to define this in the scss instead?
+      height: '',
+    }
+  }
 }
